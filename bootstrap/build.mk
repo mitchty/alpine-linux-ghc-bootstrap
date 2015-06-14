712c26e6295eaae97704ed50339756415bb4f0ad
Mitch Tishmack <mitch.tishmack@gmail.com>
2015-06-14 16:59:00 -0500

    Old kinda working, need to test out removal of settings changes.
    Then will make sure I can still build without -fPIC stuff as I
    appear to have been hitting PaX related issues which this glossed over.

---
+++ bootstrap/build.mk
@@ -0,0 +1,17 @@
+SRC_HC_OPTS          = -H64m -O0 -fPIC
+GhcStage1HcOpts      = -O -fPIC
+GhcStage2HcOpts      = -O0 -fPIC
+GhcLibHcOpts         = -O -fPIC
+GhcHcOpts            = -Rghc-timing
+SplitObjs            = NO
+BeConservative       = YES
+SRC_CC_OPTS          = -fPIC
+HADDOCK_DOCS         = NO
+BUILD_DOCBOOK_HTML   = NO
+BUILD_DOCBOOK_PS     = NO
+BUILD_DOCBOOK_PDF    = NO
+INTEGER_LIBRARY      = integer-simple
+DYNAMIC_BY_DEFAULT   = NO
+DYNAMIC_GHC_PROGRAMS = NO
+GhcFAsm              = $(if $(filter $(GhcWithNativeCodeGen),YES),-fasm,)
+V                    = 1
